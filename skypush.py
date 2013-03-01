797b47062dd51860f4949526e29ab78c61000d49
Craig Wright <craig@wright.name>
2013-03-01 15:14:05 -0800

    Added send to tag, cleaned requirements.txt.

--- skypush.py
+++ skypush.py
@@ -8,7 +8,10 @@ import config
 
 app_key = config.app_key
 master_secret = config.master_secret
-alias = 'crw'
+# aliases = ['crw']
+# tags = ['crw']
+aliases = None
+tags = None
 uri = 'https://go.urbanairship.com/api/push/'
 
 log_file = 'C:\Users\crw\Documents\My Games\Skyrim\Logs\Script\User\PushLog.0.log'
@@ -16,27 +19,43 @@ log_file = 'C:\Users\crw\Documents\My Games\Skyrim\Logs\Script\User\PushLog.0.lo
 push_message = 'Skyrim: Craig checked in to {}.'
 
 
-def send_push(message, alias=None, dryrun=False):
+def send_push(message, aliases=None, tags=None, dryrun=False):
+    send_uri = uri
     content = {}
-    if alias:
-        content['aliases'] = [alias]
+    if aliases:
+        content['aliases'] = aliases
+    if tags:
+        content['tags'] = tags
+    if aliases is None and tags is None:
+        send_uri = uri + '/broadcast/'
+
     content['aps'] = {
         "badge": "+1",
         "alert": message,
         "sounds": "cat.caf",
     }
+    content['android'] = {
+        "alert": message
+    }
     headers = {'Content-type': 'application/json'}
     if not dryrun:
         resp = requests.post(
-            uri, 
+            send_uri, 
             auth=(app_key, master_secret), 
             data=json.dumps(content),
             headers=headers
         )
-    if alias is None:
-        alias = 'everybody'
+    if aliases is None and tags is None:
+        aliases = 'everybody'
+        tags = 'everybody'
+    if aliases is None:
+        aliases = 'None'
+    if tags is None:
+        tags = 'None'
 
-    print 'Sent push "{}" to {}.'.format(message, alias)
+
+    print 'Sent push "{}" to aliases: {}, tags: {}.'\
+        .format(message, aliases, tags)
     if not dryrun:
         print "{} {}".format(resp.status_code, resp.text)
 
@@ -56,7 +75,7 @@ def watch_log_file(log_file, dryrun=False):
             print "Sending {}".format(cur_line)
             if cur_line[26:45] == '[SkyPush][Location]':
                 message = push_message.format(cur_line[46:])
-                send_push(message, alias, dryrun=dryrun)
+                send_push(message, aliases=aliases, tags=tags, dryrun=dryrun)
         cur_line = ''
         time.sleep(5)
 
